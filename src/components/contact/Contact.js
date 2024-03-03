c821c3adf5163ff5e6f2ddd498861c25e356703e
praveenkaikala <praveenkaikala25@gmail.com>
2024-03-03 11:38:41 +0530

    last

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,36 +1,70 @@
-import React,{useRef} from 'react'
+import React, { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
+import { Formik, Form, Field,ErrorMessage } from 'formik';
+import './contact.css'; // Your custom CSS file
+import * as yup from 'yup';
 
-import './contact.css'
 
+import { Alert, Button, TextField } from '@mui/material';
 
 const Contact = () => {
-  const form = useRef();
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const ref = useRef();
+  const [send,setSend]=useState(false)
+  const validationSchema = yup.object({
+    user_name: yup.string().required('Name is required'),
+    user_email: yup.string().email('Invalid email').required('Email is required'),
+    message: yup.string().required('Message is required'),
+  });
 
-    emailjs.sendForm('service_sq7j3e8', 'template_2s3dgic', form.current, 'SxTnRoUmxsMcJHqPz')
-      .then( (error) => {
-          console.log(error.text);
+  const sendEmail = () => {
+    emailjs
+      .sendForm('service_sq7j3e8', 'template_2s3dgic', ref.current, 'SxTnRoUmxsMcJHqPz')
+      .then(() => {
+        console.log("Email sent successfully");
+        setSend(true)
+        setTimeout(() => {
+          setSend(false);
+        }, 2000)
+      })
+      .catch((err) => {
+        console.error("Error sending email:", err);
       });
-      e.target.reset();
   };
+
   return (
-    <section className='contact' id='contact'>
-      <h3>contact me</h3>
-      <div className='contactbox'>
-      <form ref={form} onSubmit={sendEmail}>
-      <label>Name</label>
-      <input type="text" name="user_name" />
-      <label>Email</label>
-      <input type="email" name="user_email" />
-      <label>Message</label>
-      <textarea name="message" className='msgbox' />
-      <input type="submit" className='btn' value="Send" />
-    </form>
+    <section className="contact" id="contact1">
+      <div className="contactbox">
+        <h3>Contact me</h3>
+        <div style={{ width: "50%", backgroundColor: "white", margin: "10px", borderRadius: "20px" }}>
+          <Formik
+            initialValues={{
+              user_name: '',
+              user_email: '',
+              message: '',
+            }}
+            
+            onSubmit={(values,{resetForm})=>{
+              sendEmail()
+              resetForm()
+            }}
+          >
+            <Form ref={ref} style={{ margin: "10px", display: "flex", flexDirection: "column", gap: "10px", zIndex: -1 }}>
+              <TextField name="user_name" id="outlined-basic" label="Name" variant="outlined" />
+              <ErrorMessage name="user_name" component="div" className="error" style={{color:"red"}}/>
+              <TextField name="user_email" id="outlined-basic" label="Email" type='email' variant="outlined" />
+              <ErrorMessage name="user_email" component="div" className="error" style={{color:"red"}}/>
+              <TextField id="outlined-multiline-flexible" label="Message" multiline maxRows={4} name="message" variant="outlined" />
+              <ErrorMessage name="message" component="div" className="error" style={{color:"red"}}/>
+              <Button variant="contained" type="submit">Send</Button>
+              <p style={{color:"blue"}}>
+                {send ? "email send successfully" : ""}
+              </p>
+            </Form>
+          </Formik>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './projects.css'
+import chat from "../../asserts/chat.png"
 
 const Projects = () => {
+  const [projects,setprojects]=useState([
+    {
+      name:"chatting application",
+      link:"https://capable-creponne-5f89a0.netlify.app/",
+      image:chat
+    },
+  ])
   return (
     <section className='project' id='project'>
       <h3>my projects</h3>
       <div className='container'>
-         <div className='projectbox'>
-         </div>
+          {projects.map((project,index)=>{
+            return(
+              <div key={index} style={{display:"flex",justifyContent:"center",cursor:"pointer"}}>
+                 <div className='projectbox'>
+                <a style={{textDecoration:"none"}} href={project.link}>
+                <img src={project.image} />
+              <p style={{color:"white"}}>{project.name}</p>
 
-      </div>
+                </a>
+              
+
+              </div>
+                </div>
+             
+            )
+          })}
+         </div>
     </section>
   )
 }
