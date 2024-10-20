f4efffb1aa39344417c839c15bfaecd5a9ebeabf
praveenkaikala <142200525+praveenkaikala@users.noreply.github.com>
2024-10-20 15:06:08 +0530

    changing home component

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,25 +1,35 @@
-import React from 'react'
-import "./home.css"
-import { easeInOut, motion } from 'framer-motion'
-import profile from '../../asserts/profile.png'
+import React from 'react';
+import './home.css';
+import { easeInOut, motion } from 'framer-motion';
+import profile from '../../asserts/profile.png';
+
 const Home = () => {
   return (
-    <section className='homesection' id='home' >
-        <motion.div initial={{scale: 0.8}} animate={{scale:1}} transition={{duration:1,ease:easeInOut}}
-        className='home'>
-            <div className='textpart'>
-          
-              <h3>hello</h3>
-              <p>i am <span className='name'>praveen </span></p>
-              <span>full stack developer</span>
-              
-            </div>
-            <div className='imagepart'>
-                <img src={profile} alt='profile'/>
-            </div>
+    <section className='homesection' id='home'>
+      <motion.div
+        initial={{ scale: 0.8, opacity: 0 }}
+        animate={{ scale: 1, opacity: 1 }}
+        transition={{ duration: 1, ease: easeInOut }}
+        className='home'
+      >
+        <div className='textpart'>
+          <h3>Hello,</h3>
+          <p>
+            I am <span className='name'>Praveen</span>
+          </p>
+          <span className='role'>Full Stack Developer</span>
+        </div>
+        <motion.div
+          className='imagepart'
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, ease: easeInOut }}
+        >
+          <img src={profile} alt='profile' />
         </motion.div>
+      </motion.div>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
